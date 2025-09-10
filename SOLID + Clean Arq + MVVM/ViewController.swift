0f4ba070cc9a8b436b30559dacf476f7611c00d8
ricardo sanchez <ricardo.sanchez@applydigital.com>
2025-09-10 15:53:43 -0400

    Initial Commit

---
+++ SOLID + Clean Arq + MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID + Clean Arq + MVVM
+//
+//  Created by ricardo sanchez on 10/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
